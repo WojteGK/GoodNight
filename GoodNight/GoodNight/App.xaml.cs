9c143fdb37fec51241e755f5df7e9da9703266fc
Kusieqs <130232383+Kusieqs@users.noreply.github.com>
2023-10-20 18:42:48 +0200

    Dodaj pliki projektów.

---
+++ GoodNight/GoodNight/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using GoodNight.Services;
+using GoodNight.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GoodNight
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
