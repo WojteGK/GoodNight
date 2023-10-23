804215b29bf4cf4712e43c5f0852e52cf6d03ee8
bartek-korzen <bartoszkorzen.gte@gmail.com>
2023-10-23 18:45:51 +0200

    Added Homepage with a working button

--- GoodNight/GoodNight/ViewModels/HomeViewModel.cs
+++ GoodNight/GoodNight/ViewModels/HomeViewModel.cs
@@ -5,11 +5,11 @@ using Xamarin.Forms;
 
 namespace GoodNight.ViewModels
 {
-    public class AboutViewModel : BaseViewModel
+    public class HomeViewModel : BaseViewModel
     {
-        public AboutViewModel()
+        public HomeViewModel()
         {
-            Title = "About";
+            Title = "GoodNight";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
 
---
+++ GoodNight/GoodNight/Views/HomePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GoodNight.Views
+{
+    public partial class HomePage : ContentPage
+    {        
+
+        public HomePage()
+        {           
+            InitializeComponent();
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
