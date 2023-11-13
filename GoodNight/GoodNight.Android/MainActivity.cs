d92e93177ee09c142a4ee58747425f1ee4721d9e
Kusieqs <konrad.kus@edu.uekat.pl>
2023-11-13 16:23:44 +0100

    set sound in notification

--- GoodNight/GoodNight.Android/MainActivity.cs
+++ GoodNight/GoodNight.Android/MainActivity.cs
@@ -1,21 +1,23 @@
-﻿using System;
-using Plugin.LocalNotification;
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
 using Android.OS;
+using Android.Runtime;
+using Plugin.LocalNotification;
 
 namespace GoodNight.Droid
 {
-    [Activity(Label = "GoodNight", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "GoodNight", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            LocalNotificationCenter.MainActivity= this;
-            LocalNotificationCenter.CreateNotificationChannel();
+            LocalNotificationCenter.MainActivity = this;
+            LocalNotificationCenter.CreateNotificationChannel(new Plugin.LocalNotification.AndroidOption.NotificationChannelRequest
+            {
+                Sound = Resource.Raw.sound.ToString()
+            });
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
